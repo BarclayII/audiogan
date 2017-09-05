ce4076b3ac8a9f40c6aa41d5c66a0e979b225a86
Quan Gan <coin2028@hotmail.com>
2017-09-05 16:23:59 -0400

    can run now

--- audiogan.py
+++ audiogan.py
@@ -18,7 +18,7 @@ import dataset
 import utiltf as util
 
 def tovar(*arrs):
-    tensors = [T.Tensor(a).cuda() for a in arrs]
+    tensors = [(T.Tensor(a.astype('float32')) if isinstance(a, NP.ndarray) else a).cuda() for a in arrs]
     vars_ = [T.autograd.Variable(t) for t in tensors]
     return vars_[0] if len(vars_) == 1 else vars_
 
@@ -29,9 +29,9 @@ def tonumpy(*vars_):
 
 
 def div_roundup(x, d):
-    return (x + d - 1) // d
+    return (x + d - 1) / d
 def roundup(x, d):
-    return (x + d - 1) // d * d
+    return (x + d - 1) / d * d
 
 
 def log_sigmoid(x):
@@ -43,20 +43,46 @@ def log_one_minus_sigmoid(x):
     return x_sign * y_pos + (1 - x_sign) * y_neg
 
 
+def binary_cross_entropy_with_logits_per_sample(input, target, weight=None):
+    if not target.is_same_size(input):
+        raise ValueError("Target size ({}) must be the same as input size ({})".format(target.size(), input.size()))
+
+    max_val = (-input).clamp(min=0)
+    loss = input - input * target + max_val + ((-max_val).exp() + (-input - max_val).exp()).log()
+
+    if weight is not None:
+        loss = loss * weight
+
+    return loss.sum(1)
+
+
+def advanced_index(t, dim, index):
+    return t.transpose(dim, 0)[index].transpose(dim, 0)
+
+
+def length_mask(size, length):
+    batch_size = size[0]
+    weight = T.zeros(*size)
+    for i in range(batch_size):
+        weight[i, :length[i]] = 1.
+    weight = tovar(weight)
+    return weight
+
+
 def dynamic_rnn(rnn, seq, length, initial_state):
-    length_sorted, length_sorted_idx = T.sort(length)
+    length_sorted, length_sorted_idx = T.sort(length, descending=True)
     _, length_inverse_idx = T.sort(length_sorted_idx)
     rnn_in = pack_padded_sequence(
-            seq[:, length_sorted_idx],
-            to_numpy(length_sorted),
+            advanced_index(seq, 1, length_sorted_idx),
+            tonumpy(length_sorted),
             )
     rnn_out, rnn_last_state = rnn(rnn_in, initial_state)
     rnn_out = pad_packed_sequence(rnn_out)[0]
-    out = rnn_out[:, length_inverse_idx]
+    out = advanced_index(rnn_out, 1, length_inverse_idx)
     if isinstance(rnn_last_state, tuple):
-        state = tuple(s[:, length_inverse_idx] for s in rnn_last_state)
+        state = tuple(advanced_index(s, 1, length_inverse_idx) for s in rnn_last_state)
     else:
-        state = s[:, length_inverse_idx]
+        state = advanced_index(s, 1, length_inverse_idx)
 
     return out, state
 
@@ -79,6 +105,7 @@ class Embedder(NN.Module):
                  num_layers=1,
                  num_chars=256,
                  ):
+        NN.Module.__init__(self)
         self._output_size = output_size
         self._char_embed_size = char_embed_size
         self._num_layers = num_layers
@@ -86,7 +113,7 @@ class Embedder(NN.Module):
         self.embed = NN.Embedding(num_chars, char_embed_size)
         self.rnn = NN.LSTM(
                 char_embed_size,
-                output_size,
+                output_size // 2,
                 num_layers,
                 bidirectional=True,
                 )
@@ -98,12 +125,12 @@ class Embedder(NN.Module):
 
         embed_seq = self.embed(chars).permute(1, 0, 2)
         initial_state = (
-                tovar(T.zeros(num_layers * 2, batch_size, state_size)),
-                tovar(T.zeros(num_layers * 2, batch_size, state_size)),
+                tovar(T.zeros(num_layers * 2, batch_size, output_size // 2)),
+                tovar(T.zeros(num_layers * 2, batch_size, output_size // 2)),
                 )
         embed, (h, c) = dynamic_rnn(self.rnn, embed_seq, length, initial_state)
         h = h.permute(1, 0, 2)
-        return h[:, -2:].view(batch_size, output_size * 2)
+        return h[:, -2:].view(batch_size, output_size)
 
 
 class Generator(NN.Module):
@@ -114,6 +141,7 @@ class Generator(NN.Module):
                  state_size=1024,
                  num_layers=1,
                  ):
+        NN.Module.__init__(self)
         self._frame_size = frame_size
         self._noise_size = noise_size
         self._state_size = state_size
@@ -146,43 +174,41 @@ class Generator(NN.Module):
         lstm_h = [tovar(T.zeros(batch_size, state_size)) for _ in range(num_layers)]
         lstm_c = [tovar(T.zeros(batch_size, state_size)) for _ in range(num_layers)]
         x_t = tovar(T.zeros(batch_size, frame_size))
-        generating = T.ones(batch_size).byte()
+        generating = T.ones(batch_size).long()
         length = T.zeros(batch_size).long()
 
         x_list = []
         s_list = []
-        log_action_list = []
+        stop_list = []
         for t in range(nframes):
             z_t = z[:, t]
-            c_t = c[:, t]
-            _x = T.cat([x_t, z_t, c_t], 1)
-            lstm_h[0], lstm_c[0] = self.rnn(_x, (lstm_h, lstm_c))
+            _x = T.cat([x_t, z_t], 1)
+            lstm_h[0], lstm_c[0] = self.rnn[0](_x, (lstm_h[0], lstm_c[0]))
             for i in range(1, num_layers):
-                lstm_h[i], lstm_c[i] = self.rnn(lstm_h[i-1], (lstm_h[i], lstm_c[i]))
-            x = self.proj(lstm_h[-1]).tanh_()
-            logit_s = self.stopper(lstm_h)
-            s = log_sigmoid(logit_s)
-            s1 = log_one_minus_sigmoid(logit_s)
+                lstm_h[i], lstm_c[i] = self.rnn[i](lstm_h[i-1], (lstm_h[i], lstm_c[i]))
+            x_t = self.proj(lstm_h[-1]).tanh_()
+            logit_s_t = self.stopper(lstm_h[-1])
+            s_t = log_sigmoid(logit_s_t)
+            s1_t = log_one_minus_sigmoid(logit_s_t)
 
-            logp = T.cat([s1, s], 1)
-            p = logp.exp()
-            stop = p.multinomial()
-            log_action = logp.gather(1, stop) * generating
-            length += generating.long()
+            logp_t = T.cat([s1_t, s_t], 1)
+            p_t = logp_t.exp()
+            stop_t = p_t.multinomial()
+            length += generating
 
-            x_list.append(x)
-            s_list.append(s)
-            log_action_list.append(log_action)
+            x_list.append(x_t)
+            s_list.append(logit_s_t.squeeze())
+            stop_list.append(stop_t)
 
-            generating *= (stop == 0)
-            if generating.data.sum() == 0:
+            stop_t = stop_t.squeeze()
+            generating *= (stop_t.data == 0).long().cpu()
+            if generating.sum() == 0:
                 break
 
-        x = T.cat(x, 1)
-        s = T.stack(s, 1)
-        log_action = T.stack(log_action_list, 1).sum(1)
+        x = T.cat(x_list, 1)
+        s = T.stack(s_list, 1)
 
-        return x, s, log_action, tovar(length * frame_size)
+        return x, s, stop_list, tovar(length * frame_size)
 
 
 class Discriminator(NN.Module):
@@ -191,6 +217,7 @@ class Discriminator(NN.Module):
                  state_size=1024,
                  embed_size=200,
                  num_layers=1):
+        NN.Module.__init__(self)
         self._frame_size = frame_size
         self._state_size = state_size
         self._embed_size = embed_size
@@ -198,7 +225,7 @@ class Discriminator(NN.Module):
 
         self.rnn = NN.LSTM(
                 frame_size + embed_size,
-                state_size,
+                state_size // 2,
                 num_layers,
                 bidirectional=True,
                 )
@@ -215,19 +242,20 @@ class Discriminator(NN.Module):
         embed_size = self._embed_size
         batch_size, maxlen = x.size()
         max_nframes = div_roundup(maxlen, frame_size)
-        nframes = length // frame_size
+        nframes = div_roundup(length, frame_size)
 
         x = x.view(batch_size, max_nframes, frame_size)
         c = c.unsqueeze(1).expand(batch_size, max_nframes, embed_size)
         x = T.cat([x, c], 2).permute(1, 0, 2)
 
         initial_state = (
-                tovar(T.zeros(num_layers * 2, batch_size, state_size)),
-                tovar(T.zeros(num_layers * 2, batch_size, state_size)),
+                tovar(T.zeros(num_layers * 2, batch_size, state_size // 2)),
+                tovar(T.zeros(num_layers * 2, batch_size, state_size // 2)),
                 )
 
         lstm_out, (lstm_h, lstm_c) = dynamic_rnn(self.rnn, x, nframes, initial_state)
-        lstm_out = lstm_out[nframes_inverse_idx].permute(1, 0, 2)
+        lstm_out = lstm_out.permute(1, 0, 2)
+        max_nframes = lstm_out.size()[1]
 
         classifier_in = lstm_out.view(batch_size * max_nframes, state_size)
         classifier_out = self.classifier(classifier_in).view(batch_size, max_nframes)
@@ -258,6 +286,7 @@ parser.add_argument('--minwordlen', type=int, default=1)
 parser.add_argument('--maxlen', type=int, default=0)
 
 args = parser.parse_args()
+args.conditional = True
 
 if args.just_run not in ['', 'gen', 'dis']:
     print('just run should be empty string, gen, or dis. Other values not accepted')
@@ -294,7 +323,7 @@ _, cseq_fixed, clen_fixed = dataset.pick_words(batch_size, args)
 
 d = Discriminator(
         frame_size=args.framesize,
-        state_size=args.statesize,
+        state_size=args.dstatesize,
         embed_size=args.embedsize,
         num_layers=args.rnnd_layers,
         ).cuda()
@@ -311,10 +340,10 @@ baseline = 0.
 param_g = list(g.parameters()) + list(e_g.parameters())
 param_d = list(d.parameters()) + list(e_d.parameters())
 
-opt_g = T.optim.RMSprop(param_g)
-opt_d = T.optim.RMSprop(param_d)
+opt_g = T.optim.SGD(param_g, lr=1e-5)
+opt_d = T.optim.SGD(param_d, lr=1e-5)
 if __name__ == '__main__':
-    if moddelnameload:
+    if modelnameload:
         if len(modelnameload) > 0:
             T.load(d, '%s-dis-%05d' % (modelnameload, args.loaditerations))
             T.load(g, '%s-gen-%05d' % (modelnameload, args.loaditerations))
@@ -327,6 +356,7 @@ if __name__ == '__main__':
         for j in range(args.critic_iter):
             with Timer.new('load', print_=False):
                 epoch, batch_id, real_data, real_len, _, cs, cl, _, csw, clw = dataloader.next()
+                _cs2, cl2 = dataset.pick_words(batch_size, args)
 
             with Timer.new('train_d', print_=False):
                 real_data = tovar(real_data + RNG.randn(*real_data.shape))
@@ -337,47 +367,53 @@ if __name__ == '__main__':
                 embed_d = e_d(cs, cl)
                 cls = d(real_data, real_len, embed_d)
                 target = tovar(T.ones(*(cls.size())))
-                loss = F.binary_cross_entropy_with_logits(cls, target)
+                weight = length_mask(cls.size(), div_roundup(real_len.data, args.framesize))
+                loss = F.binary_cross_entropy_with_logits(cls, target, weight=weight, size_average=False)
 
-                _, cs, cl = dataset.pick_words(batch_size, args)
-                cs = tovar(cs).long()
-                cl = tovar(cl).long()
-                embed_g = e_g(cs, cl)
-                embed_d = e_d(cs, cl)
+                cs2 = tovar(cs2).long()
+                cl2 = tovar(cl2).long()
+                embed_g = e_g(cs2, cl2)
+                embed_d = e_d(cs2, cl2)
                 fake_data, _, _, fake_len = g(batch_size=args.batchsize, length=maxlen, c=embed_g)
-                fake_data += T.autograd.Variable(T.randn(*fake_data.size()))
+                fake_data += tovar(T.randn(*fake_data.size()))
                 cls = d(fake_data, fake_len, embed_d)
                 target = tovar(T.zeros(*(cls.size())))
-                loss += F.binary_cross_entropy_with_logits(cls, target)
+                weight = length_mask(cls.size(), div_roundup(fake_len.data, args.framesize))
+                loss += F.binary_cross_entropy_with_logits(cls, target, weight=weight, size_average=False)
 
                 opt_d.zero_grad()
                 loss.backward()
                 check_grad(param_d)
                 opt_d.step()
 
-            print 'D', epoch, batch_id, to_numpy(loss), Timer.get('load'), Timer.get('train_d')
+            print 'D', epoch, batch_id, tonumpy(loss), Timer.get('load'), Timer.get('train_d')
 
         i += 1
         for p in param_g:
             p.requires_grad = True
 
+        _, cs, cl = dataset.pick_words(batch_size, args)
         with Timer.new('train_g', print_=False):
-            _, cs, cl = dataset.pick_words(batch_size, args)
             cs = tovar(cs).long()
             cl = tovar(cl).long()
             embed_g = e_g(cs, cl)
             embed_d = e_d(cs, cl)
-            fake_data, fake_stop, fake_logprob, fake_len = g(batch_size=args.batchsize, length=maxlen, c=embed_g)
-            fake_data += T.autograd.Variable(T.randn(*fake_data.size()))
+            fake_data, fake_s, fake_stop_list, fake_len = g(batch_size=args.batchsize, length=maxlen, c=embed_g)
+            fake_data += tovar(T.randn(*fake_data.size()))
             cls = d(fake_data, fake_len, embed_d)
             target = tovar(T.ones(*(cls.size())))
-            loss = F.binary_cross_entropy_with_logits(cls, target)
-            reward = -to_numpy(loss)[0]
-            baseline = baseline * 0.999 + reward * 0.001
-            cost_action = F.cross_entropy(fake_logprob, fake_stop)
-            loss += alpha * (reward - baseline) * cost_action
+            weight = length_mask(cls.size(), div_roundup(fake_len.data, args.framesize))
+            loss = binary_cross_entropy_with_logits_per_sample(cls, target, weight=weight)
+
+            reward = -loss.data
+            baseline = baseline * 0.999 + reward.mean() * 0.001
+            reward = (reward - baseline).unsqueeze(1) * weight.data
+            loss = loss.mean()
+            for i, fake_stop in enumerate(fake_stop_list):
+                fake_stop.reinforce(reward[:, i:i+1])
             opt_g.zero_grad()
-            loss.backward()
+            loss.backward(retain_graph=True)
+            T.autograd.backward(fake_stop_list, [None for _ in fake_stop_list])
             opt_g.step()
 
         print 'G', i, loss, Timer.get('train_g')
